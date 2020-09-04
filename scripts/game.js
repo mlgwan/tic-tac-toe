601125486133c93a10fb62f94c7a378e485cef1d
Ciwan Clifford <theciwan@gmail.com>
2020-09-04 10:20:01 +0200

    Clicking on a field will now display a symbol, depending on whose players turn it is. Fields can not be filled twice, but there is no gamelogic yet.

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,5 @@
 const gameBoard = (function(){
-    let board = ["x","o","x","o","x","o","x","o","o"];
+    let board = [];
     const boardElement = Array.from(document.getElementById("board").children);
 
     const getBoard = ()=> board;
@@ -22,15 +22,42 @@ const gameBoard = (function(){
 const gameController = (function(){
 
 
-    return {
+    const startGame = (player1, player2, board)=>{
+        const fields = Array.from(document.querySelectorAll(".field"));
+        const players = [player1, player2];
+        let currentPlayer = 0;
+        fields.forEach((field, index)=>{
+            field.addEventListener("click", ()=>{
+                if (board.getBoard()[index] === undefined){
+                    board.setBoard(players[currentPlayer].getSymbol(), index);
+                    board.render();
+                    currentPlayer++;
+                    currentPlayer%=2;
+                }
+                
 
+            })
+        })
+    }
+
+
+
+    return {
+        startGame
     }
 })();
 
-const playerFactory = (function(name){
-
-
+const playerFactory = (function(name, color, symbol){
+    const getName = ()=>name;
+    const getColor = ()=>color;
+    const getSymbol = ()=>symbol;
     return {
-
+        getName,
+        getColor,
+        getSymbol
     }
-})();
+});
+
+const player1 = playerFactory("Player1", "#F00", "x");
+const player2 = playerFactory("Player2", "#00F", "o");
+gameController.startGame(player1, player2, gameBoard);
