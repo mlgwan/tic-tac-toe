2475a5e3d048ae6e99f056eb1caccf67f20840cd
Ciwan Clifford <theciwan@gmail.com>
2020-09-04 15:08:42 +0200

    Games can end now and a message is alerted notifying the winner or in case the game ends in a draw.

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,9 @@
 const gameBoard = (function(){
     let board = [];
+    const boardSize = 9;
+    const boardWidth = Math.sqrt(boardSize);
+    let emptyFields;
+
     const boardElement = Array.from(document.getElementById("board").children);
 
     const getBoard = ()=> board;
@@ -12,10 +16,59 @@ const gameBoard = (function(){
         })
     }
 
+    const checkForGameOver = ()=>{
+        if(checkRows(board) || checkColumns(board) || checkDiagonals(board)){
+            return "win";
+        }
+        else if (emptyFields === 0){
+            return "draw";
+        }
+    }
+
+    const checkRows = board=>{
+        for (let i = 0; i < boardSize; i+=boardWidth){
+            if (board[i] !== "" && board[i] === board[i+1] && board[i] === board[i+2]){
+                return true;
+            }
+        }
+        return false;   
+    }
+
+    const checkColumns = board=>{
+        for (let i = 0; i < boardWidth; i++){
+            if (board[i] !== "" && board[i] === board[i+boardWidth] && board[i] === board[i+2*boardWidth]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkDiagonals = board=>{
+        if ((board[0] !== "" && board[0] === board[boardWidth+1] && board[0] === board[2*(boardWidth+1)]) ||
+            (board[boardWidth-1] !== "" && board[boardWidth-1] === board[(boardWidth-1) + (boardWidth-1)] && board[boardWidth-1] === board[(boardWidth-1) + 2*(boardWidth-1)])){
+                return true;
+        }
+        return false;
+    }
+
+    const initBoard = ()=>{
+        for (let i = 0; i < boardSize; i++){
+            board[i] = "";
+        }
+        emptyFields = boardSize;
+    }
+
+    const decrementEmptyFields = ()=>{
+        emptyFields--;
+    }
+
     return {
         getBoard,
         setBoard,
-        render
+        render,
+        checkForGameOver,
+        initBoard,
+        decrementEmptyFields
     }
 })();
 
@@ -25,15 +78,21 @@ const gameController = (function(){
     const startGame = (player1, player2, board)=>{
         const fields = Array.from(document.querySelectorAll(".field"));
         const players = [player1, player2];
+        board.initBoard();
         let currentPlayer = 0;
         fields.forEach((field, index)=>{
             field.addEventListener("click", ()=>{
-                if (board.getBoard()[index] === undefined){
+                if (board.getBoard()[index] === ""){
                     board.setBoard(players[currentPlayer].getSymbol(), index);
                     board.render();
-                    field.children[0].color = players[currentPlayer].getColor();
+                    board.decrementEmptyFields();
                     field.children[0].style.color = players[currentPlayer].getColor();
-    
+                    if(board.checkForGameOver() === "win"){
+                        alert(players[currentPlayer].getName() +" (" + players[currentPlayer].getSymbol() + ") has won!");
+                    }
+                    else if (board.checkForGameOver() === "draw"){
+                        alert("It's a draw!");
+                    }
                     currentPlayer++;
                     currentPlayer%=2;
                 }
